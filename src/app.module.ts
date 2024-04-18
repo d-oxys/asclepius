eb5b2a38144d93a6bee249ca37139e8c61563302
D-oxys <sanuzumaki12@gmail.com>
2024-04-18 08:05:09 +0700

    feat:add predict

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PredictController } from './predict/predict.controller';
+import { ModelService } from './predict/predict.service';
 
 @Module({
   imports: [],
   controllers: [AppController, PredictController],
-  providers: [AppService], // Add PredictService to providers
+  providers: [AppService, ModelService],
 })
 export class AppModule {}
